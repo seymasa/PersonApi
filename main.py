8290093638e6c4a38545ad8580af709f77f1c64f
Seyma SARIGİL <seymasargil@gmail.com>
2022-01-27 02:33:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from typing import Optional
 
-from fastapi import FastAPI, Body
+from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
-persons = [{'id': 1, 'ad': 'Seyma', 'soyad': 'Sarıgil', 'meslek': 'Gelistirici', 'memleket': 'Hatay'},
-           {'id': 2, 'ad': 'Alp', 'soyad': 'Kara', 'meslek': 'Müzisyen', 'memleket': 'İstanbul'}]
+persons = [{"id": 1, "ad": "Seyma", "soyad": "Sarıgil", "meslek": "Gelistirici", "memleket": "Hatay"},
+           {"id": 2, "ad": "Alp", "soyad": "Kara", "meslek": "Müzisyen", "memleket": "İstanbul"}]
 
 
 class Person(BaseModel):
@@ -24,26 +24,26 @@ def get_all_persons():
 @app.post("/add_person/")
 def add_person(person: Person): #ad, soyad, meslek, memleket
     person_dict = person.dict()
-    person_dict['id'] = persons[-1]['id'] + 1
+    person_dict["id"] = persons[-1]["id"] + 1
     persons.append(dict(person_dict))
     return persons
 
 
 @app.get("/get_person/{person_id}")
 def get_specific_person(person_id: int):
-    person = [i for i in persons if i['id'] == person_id]
+    person = [i for i in persons if i["id"] == person_id]
     return person
 
 
 @app.put("/update_person/{person_id}")
 def update_name_person(person_id: int, person: Person):
     p = [i for i in persons if person.id == person_id]
-    p[0]['ad'] = person.ad
+    p[0]["ad"] = person.ad
     return p
 
 
 @app.delete("/delete_person/{person_id}")
 def delete_person(person_id: int):
-    person = [i for i in persons if i['id'] == person_id]
+    person = [i for i in persons if i["id"] == person_id]
     persons.remove(person[0])
     return person
